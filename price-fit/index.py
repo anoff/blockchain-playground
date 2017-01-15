e3dc2e754ff9fae6dc762ce6a40922d0bc86f1ff
anoff <offenhaeuser@gmail.com>
2017-01-15 01:21:40 +0100

    :star2: feat: multiple lingress forecasts

--- price-fit/index.py
+++ price-fit/index.py
@@ -2,13 +2,15 @@ import requests
 import matplotlib.pyplot as plt
 import datetime
 from scipy import stats
+import numpy as np
 import numpy
 
 start_date = '2013-01-01'
-regression_ranges = [7, 30, 90, 365]
+end_date = datetime.date.today().isoformat()
+regression_ranges = [30, 90] # ranges in days that should show up as individual linear regressions
 
 r = requests.get('http://api.coindesk.com/v1/bpi/historical/close.json?currency=EUR&start={1}&end={0}'
-.format(datetime.date.today().isoformat(), start_date))
+.format(end_date, start_date))
 data = r.json()['bpi']
 x = list(data.keys())
 x.sort()
@@ -17,14 +19,13 @@ y = [data[e] for e in x]
 t = [datetime.datetime.strptime(elm, '%Y-%m-%d') for elm in x]
 dt = [(elm - t[0]).days for elm in t]
 
-
-print(x.index('2015-01-01'))
-
-exit()
-
 # linear regression
 slope, intercept, r_value, p_value, std_err = stats.linregress(dt, y)
-# print(slope, intercept, r_value, p_value, std_err)
+
+# polyfit 1D
+coeff = np.polyfit(dt, y, 2)
+print(coeff)
+fit = [coeff[0] * (e ** 2) + coeff[1] * e + coeff[2] for e in dt]
 
 # regression at random point
 def regY(x):
@@ -33,8 +34,6 @@ def regY(x):
 # std error
 errors = [y[i] - regY(i) for i in dt]
 reg_error = numpy.mean(numpy.absolute(errors))
-# print(reg_error)
-#exit()
 
 # calculate price in one year
 forecast = [30, 90, 180, 365] # forecast in days
@@ -53,9 +52,30 @@ for val in win:
 fg, ax = plt.subplots()
 ax.plot(dt, y)
 x_min, x_max = ax.get_xlim()
-ax.plot([0, x_max], [regY(0), regY(x_max)])
-ax.plot([0, x_max], [regY(0) - reg_error, regY(x_max) - reg_error], 'r--')
-ax.plot([0, x_max], [regY(0) + reg_error, regY(x_max) + reg_error], 'r--')
+
+# 2 degree poly fit
+ax.plot(dt, fit, 'k')
+ax.plot(dt, fit + reg_error, 'k--')
+ax.plot(dt, fit - reg_error, 'k--')
+
+color=iter(plt.cm.rainbow(np.linspace(0,1,len(regression_ranges))))
+for start in regression_ranges:
+    tr = range(start)
+    yr = y[-start:]
+    sr, ir, r_value, p_value, std_err = stats.linregress(tr, yr)
+    y0 = ir
+    y1 = ir + 2 * start * sr
+    x0 = dt[-1] - start
+    x1 = dt[-1] + start
+    # print(start, sr, ir, y0, y1)
+    ax.plot([x0, x1], [y0, y1], c=next(color))
+
+
+# linear fit
+#ax.plot([0, x_max], [regY(0), regY(x_max)])
+#ax.plot([0, x_max], [regY(0) - reg_error, regY(x_max) - reg_error], 'r--')
+#ax.plot([0, x_max], [regY(0) + reg_error, regY(x_max) + reg_error], 'r--')
+
 ax.set_ylabel('price [USD]')
 ax.set_xlabel('time')
 ax.grid(True)
